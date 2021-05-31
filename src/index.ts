8c0b7c80b06fe7c8ccafcd773268f53901996d34
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-05-30 23:44:27 -0400

    setting stuff up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+const main = async () => {
+    const orm = await MikroORM.init({
+        dbName: 'STLstuff',
+        //maybe add user and pasword if this shit doesn't work
+        type:'postgresql',
+        debug: !__prod__
+    });
+};
+main();
