8f24fbf24b1cca9a714efdb360ba2399a08e0067
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-09-09 21:25:47 -0400

    popup color

--- src/App.js
+++ src/App.js
@@ -19,10 +19,12 @@ function App() {
         <div>
           {isOpen && <Popup
             content={<>
+            <div className="mainText">
               <b>This website is a work in progress</b>
               <p>STLstuff is still under development, no database (or pretty much anything) has been implemented yet. I will focus on frontend first, then take care of the database and backend side of the website. Key features that I want are a realiable search algorithm to find relevant models, adding a dicoverability feature that allows you to easiely browse 3D models that you might intersted into. The addition of a quick-download button
               </p>
               <p>Also need to add a checkmark to stop displaying this every time</p>
+              </div>
             </>}
             handleClose={togglePopup}
           />}
