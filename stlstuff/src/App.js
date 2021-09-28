11fc94a560d2ec550a448688418ab20c26ec174e
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-09-27 22:59:20 -0400

    Update App.js

--- stlstuff/src/App.js
+++ stlstuff/src/App.js
@@ -23,8 +23,8 @@ function App() {
       <Header/>
 
       <div className="Content" >
-      <p>{!data ? "Loading..." : data}</p>
-      
+      <p className="mainText">{!data ? "Loading..." : data}</p>
+
         <p className="mainText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec ante molestie, euismod lectus quis, ullamcorper justo. Sed vel leo eu velit scelerisque pellentesque. Nunc in lectus at risus ornare molestie. Sed a libero blandit, rhoncus nulla posuere, egestas ante. Etiam ullamcorper blandit erat nec placerat. Cras lectus nibh, cursus id mi ac, elementum feugiat elit. Aliquam erat volutpat. Donec laoreet vestibulum turpis, rutrum auctor neque egestas vel.
 
 Cras imperdiet posuere enim, eget cursus sem. Nulla ante nibh, varius bibendum fermentum a, ultrices vel quam. Suspendisse laoreet luctus mi, non venenatis nisl accumsan sed. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vehicula mauris est, at lacinia lacus elementum ac. Nunc non leo at sem vulputate tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed imperdiet, turpis ut venenatis malesuada, nulla ipsum hendrerit justo, at euismod metus ipsum non ante. Nulla aliquam convallis nisi eu pharetra. Sed quam ante, aliquet sed ultricies sed, feugiat a purus. In hac habitasse platea dictumst. Mauris tincidunt, tortor eget cursus vestibulum, elit orci malesuada lectus, dignissim dapibus lacus lacus at ante.
