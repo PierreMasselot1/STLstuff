37a780caa2542efea2fc0cc068e4f6c5c34a0b48
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-11-03 00:05:05 -0400

    add download and like

--- stlstuff/src/components/ObjectTile/ObjectTile.js
+++ stlstuff/src/components/ObjectTile/ObjectTile.js
@@ -1,4 +1,7 @@
 import test from '../../img/test.jpg'
+import heart from '../../img/icons/heart-line.svg'
+import download from '../../img/icons/download.svg'
+
 import "./ObjectTile.css"
 
 function ObjectTile(properties){
@@ -6,6 +9,10 @@ function ObjectTile(properties){
         <div className="objectTile">
             <h1 className="objectTitle">{properties.name}</h1>
             <img src={test} alt={"ligma balls"} className="objectImage" />
+            <div className="interactBar">
+            <img src={heart} alt={"heart icon"}/>
+            <img src={download} alt={"download icon"}/>
+            </div>
         </div>
     )   
 }
