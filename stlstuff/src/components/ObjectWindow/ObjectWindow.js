9f6789eca19352c98ba6b4b7bf689206e5fa6094
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-11-03 00:04:29 -0400

    more objects for grid testing

--- stlstuff/src/components/ObjectWindow/ObjectWindow.js
+++ stlstuff/src/components/ObjectWindow/ObjectWindow.js
@@ -5,9 +5,17 @@ function ObjectWindow(properties){
     return(
         <div className="objectWindow">
             <ObjectTile name="Object1"/>
-            <ObjectTile name="Object1"/>
-            <ObjectTile name="Object1"/>
-            <ObjectTile name="Object1"/>
+            <ObjectTile name="Object2"/>
+            <ObjectTile name="Object3"/>
+            <ObjectTile name="Object4"/>
+            <ObjectTile name="Object5"/>
+            <ObjectTile name="Object6"/>
+            <ObjectTile name="Object7"/>
+            <ObjectTile name="Object8"/>
+            <ObjectTile name="Object9"/>
+            <ObjectTile name="Object10"/>
+            <ObjectTile name="Object11"/>
+            <ObjectTile name="Object12"/>
         </div>
     )   
 }
